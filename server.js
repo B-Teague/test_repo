234f79ce20b33400cbb4df7fd69275cf140a71a1
Glitch (teague-apis-timestamp) <none>
2018-11-26 03:42:56 +0000

    ⚽️✨ Updated with Glitch

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const timestampRoute = require(__dirname + '/routes/timestampRoute');
 const indexRoute = require(__dirname + '/routes/index');
 const notFoundHandler = require(__dirname + '/middleware/notFoundHandler');
 const errorHandler = require(__dirname + '/middleware/errorHandler');
 
-app.use(cors());
 app.use(express.static('public'));
 app.use("/api/timestamp", timestampRoute);
 app.use('/', indexRoute);
